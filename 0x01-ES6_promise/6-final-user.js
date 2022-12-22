ab652db4c613c17948968dcb4fc36240bc662a28
Nathaniel <dehinbonatt@gmail.com>
2022-12-22 06:24:54 +0100

    hello

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: (result.status === 'fulfilled' ? result.value : String(result.reason)),
+    })));
+}
