3dab3bb0f19159cbb9f060cd0a9e7b1ede62bc58
Nathaniel <dehinbonatt@gmail.com>
2022-12-22 05:42:48 +0000

    pass abeg

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,15 +1,15 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-    let value;
-  
-    try {
-      value = mathFunction();
-    } catch (err) {
-      value = err.toString();
-    }
-  
-    queue.push(value);
-    queue.push('Guardrail was processed');
-  
-    return queue;
+  const queue = [];
+  let value;
+
+  try {
+    value = mathFunction();
+  } catch (err) {
+    value = err.toString();
   }
+
+  queue.push(value);
+  queue.push('Guardrail was processed');
+
+  return queue;
+}
