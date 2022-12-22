dd0921ab7be3c6a9b6f02bc3145d0802ecf787dd
Nathaniel <dehinbonatt@gmail.com>
2022-12-22 06:09:21 +0100

    hello

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-    if (numerator !== 0 && denominator !== 0) {
-        return (numerator / denominator);
-    } else if (denominator === 0) {
-        throw "cannot divide by 0"
+    if (denominator === 0) {
+      throw new Error('cannot divide by 0');
     }
-}
+    return numerator / denominator;
+  }
