8fc4f00d43d55c0c8b02a8f839296851e30dd990
Nathaniel <dehinbonatt@gmail.com>
2022-12-22 06:38:44 +0100

    hello

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,5 @@
 export default function divideFunction(numerator, denominator) {
-  if (denominator === 0) {
-    throw new Error('cannot divide by 0');
+    if (denominator === 0) throw Error('cannot divide by 0');
+  
+    return numerator / denominator;
   }
-  return numerator / denominator;
-}
->>>>>>> ebf51868f530ca62e3d5aab42f3af53964f208f3
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,15 @@
 export default function guardrail(mathFunction) {
-  const queue = [];
-  let result;
-  try {
-    result = mathFunction();
-  } catch (error) {
-    result = error.toString();
+    const queue = [];
+    let value;
+  
+    try {
+      value = mathFunction();
+    } catch (err) {
+      value = err.toString();
+    }
+  
+    queue.push(value);
+    queue.push('Guardrail was processed');
+  
+    return queue;
   }
-  queue.push(result);
-  queue.push('Guardrail was processed');
-  return queue;
-}
